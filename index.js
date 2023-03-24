40f0571859c64dbacd9637028bc71c562a01930c
ASHFAQ ISSAK <ashfaq.abdi@gmail.com>
2023-03-24 08:45:26 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+function reducer(arr) {
+    const sum = arr.reduce((acc, curr) => acc + curr, 0);
+    return sum;
+  }
+  const totalBatteries = reducer(batteryBatches);
+  console.log(totalBatteries); 
+  
